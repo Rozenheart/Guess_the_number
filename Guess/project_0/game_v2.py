fdb741da57d27c68774e96ee31d8586bc857671a
Serja <anklav.trips@gmail.com>
2022-08-15 11:25:46 +0300

    game updated, the average number of guesses is 9

--- Guess/project_0/game_v2.py
+++ Guess/project_0/game_v2.py
@@ -16,9 +16,46 @@ def random_predict(number: int=1) -> int:
     
     while True:
         count += 1
-        predict_number = np.random.randint(1, 101)
-        if predict_number == number:
-            break
+        if 1 <= number <= 10:
+            predict_number = np.random.randint(1, 11)
+            if predict_number == number:
+                break
+        elif 11 <= number <= 20:
+            predict_number = np.random.randint(11, 21)
+            if predict_number == number:
+                break     
+        elif 21 <= number <= 30:
+            predict_number = np.random.randint(21, 31)
+            if predict_number == number:
+                break     
+        elif 31 <= number <= 40:
+            predict_number = np.random.randint(31, 41)
+            if predict_number == number:
+                break
+        elif 41 <= number <= 50:
+            predict_number = np.random.randint(41, 51)
+            if predict_number == number:
+                break
+        elif 51 <= number <= 60:
+            predict_number = np.random.randint(51, 61)
+            if predict_number == number:
+                break
+        elif 61 <= number <= 70:
+            predict_number = np.random.randint(61, 71)
+            if predict_number == number:
+                break
+        elif 71 <= number <= 80:
+            predict_number = np.random.randint(71, 81)
+            if predict_number == number:
+                break
+        elif 81 <= number <= 90:
+            predict_number = np.random.randint(81, 91)
+            if predict_number == number:
+                break
+        elif 91 <= number <= 100:
+            predict_number = np.random.randint(91, 101)
+            if predict_number == number:
+                break
     return count
 
 def score_game(random_predict) -> int:
@@ -44,3 +81,4 @@ def score_game(random_predict) -> int:
 
 if __name__ == '__main__':
     score_game(random_predict)
+
