c1566d0b206daa2dd41d8632d81c9a70db2dacec
Serja <anklav.trips@gmail.com>
2022-08-19 18:54:32 +0300

    redacted code, more beauty algorithm

--- Guess/project_0/game_v2.py
+++ Guess/project_0/game_v2.py
@@ -3,60 +3,35 @@ Only PC's guesses the number"""
 
 import numpy as np
 
-def random_predict(number: int=1) -> int:
+def random_predict(number: int=np.random.randint(1, 101)) -> int:
     """Random guesses 
 
     Args:
-        number (int, optional): Guessed number. Defaults to 1.
+        number (int, optional): Guessed number. Average 1 to 100.
 
     Returns:
         int: Count of attempts
     """
+    list_of_range = list(range(number))
+    maxum = max(list_of_range)
+    minum = min(list_of_range)
+    predict_number = np.random.randint(1, 101)
     count = 0
     
     while True:
         count += 1
-        if 1 <= number <= 10:
-            predict_number = np.random.randint(1, 11)
-            if predict_number == number:
-                break
-        elif 11 <= number <= 20:
-            predict_number = np.random.randint(11, 21)
-            if predict_number == number:
-                break     
-        elif 21 <= number <= 30:
-            predict_number = np.random.randint(21, 31)
-            if predict_number == number:
-                break     
-        elif 31 <= number <= 40:
-            predict_number = np.random.randint(31, 41)
-            if predict_number == number:
-                break
-        elif 41 <= number <= 50:
-            predict_number = np.random.randint(41, 51)
-            if predict_number == number:
-                break
-        elif 51 <= number <= 60:
-            predict_number = np.random.randint(51, 61)
-            if predict_number == number:
-                break
-        elif 61 <= number <= 70:
-            predict_number = np.random.randint(61, 71)
-            if predict_number == number:
-                break
-        elif 71 <= number <= 80:
-            predict_number = np.random.randint(71, 81)
-            if predict_number == number:
-                break
-        elif 81 <= number <= 90:
-            predict_number = np.random.randint(81, 91)
-            if predict_number == number:
-                break
-        elif 91 <= number <= 100:
-            predict_number = np.random.randint(91, 101)
-            if predict_number == number:
-                break
+        if predict_number < number:
+            minum = predict_number
+            predict_number = round((minum+maxum)/2)+1
+        elif predict_number > number:
+            maxum = predict_number
+            predict_number = round((minum+maxum)/2)
+        else:
+            break
+        
     return count
+
+
 
 def score_game(random_predict) -> int:
     """Guesses for 1000 attempts 
@@ -79,6 +54,9 @@ def score_game(random_predict) -> int:
     print(f'\nAlghorythm guesses average: {score}\n')
     return 'Game over'
 
+
 if __name__ == '__main__':
     score_game(random_predict)
+
+
 
